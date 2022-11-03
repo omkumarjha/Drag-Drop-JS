2c2f7d8da94b3c7650abf1b9794852440a979512
omkumarjha <omkumar1892002@gmail.com>
2022-11-03 21:39:31 +0530

    Added the logic for dropping the draggable image

--- index.js
+++ index.js
@@ -2,12 +2,39 @@ let imageBox = document.querySelector("#imageBox")
 let boxes = document.getElementsByClassName("box")
 
 // Jaise hi draggable image ko uttayenge to drag start wala event triggored ho jayega.
-imageBox.addEventListener("dragstart",()=>{
-    console.log("Drag start has been trigored..")
+imageBox.addEventListener("dragstart",(event)=>{
+    event.target.className += " borderImage"
+    // setTimeOut 0 matlab jab sab kuch hojaye to ye class add kardena
+    setTimeout(()=>{
+        event.target.className = "hide"
+    },0)
 })
 
 // Jaise hi draggable image ko chodenge to drag end wala event triggored ho jayega.
-imageBox.addEventListener("dragend",()=>{
-    console.log("Drag end has been trigored..")
+imageBox.addEventListener("dragend",(event)=>{
+    event.target.className = "imageBox" // Matlab jaise hi hum draggable image ko chodenge to uska border gayab ho jaana chahiye.
 })
 
+for(let box of boxes){
+    
+    // dragover event tab trigger hota hai jab hum draggable image ko kisi element pe leke jaate hai.
+    box.addEventListener("dragover",(event)=>{
+        // By default hum draggable image ko kisi doose element pe drop nhi karsakte , isliye uske default behaviour ko todne ke liye yeh statement ka use kiya gaya hai.
+        event.preventDefault()
+    })
+
+    box.addEventListener("dragenter",()=>{
+        console.log("Drag enter detected..")
+    })
+
+    box.addEventListener("dragleave",()=>{
+        console.log("Drag leave detected..")
+    })
+
+    // Yeh event tab trigger hota hai jab hum draggable image ko kisi element pe laake chorte hai.
+    box.addEventListener("drop",(event)=>{
+        event.target.append(imageBox)
+    })
+
+
+}
